e5e9b156e42196d0d412bceccc9889e0eceeee21
Siromanec <serhii.ivanov@ucu.edu.ua>
2024-12-18 17:45:36 +0200

    added interface running with real interface

--- backend/service/service.py
+++ backend/service/service.py
@@ -2,11 +2,20 @@ import numpy as np
 
 from repository import convert_image_to_base64_url
 from repository.Message import Message
+from repository.abstract_chatbot import TextAndImagesChatBot
 
+qa: TextAndImagesChatBot = TextAndImagesChatBot()
+
+IMAGE_RESPONSE_TEXT = "Знайдено наступні релевантні зображення:"
 
 def generate_response(conversation: list[Message]) -> list[Message]:
-    text = qa.run_text(conversation[-1].text)
-    image = qa.run_image(conversation[-1])
-    image = convert_image_to_base64_url(image)
-    conversation.append(Message(sender="bot", text=text, imageUrl=image, conversationId=conversation[0].conversationId))
+    text, images = qa.answer_query(conversation[-1].text)
+
+    conversation.append(Message(sender="bot", text=text, conversationId=conversation[0].conversationId))
+    if len(images) == 0:
+        return conversation
+
+    conversation.append(Message(sender="bot", text=IMAGE_RESPONSE_TEXT, conversationId=conversation[0].conversationId))
+    for image in images:
+        conversation.append(Message(sender="bot", imageUrl=convert_image_to_base64_url(image), conversationId=conversation[0].conversationId))
     return conversation
